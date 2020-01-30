920115469b4eb79cf64f3610c3186a3408e08438
Valdir Mendes <valdir.mfjesus@yahoo.com.br>
2020-01-30 19:13:38 -0300

    Adding Database support

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,6 +2,8 @@ import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({message: 'It\'s ok! Server is running!'}));
+routes.get('/', (req, res) =>
+  res.json({ message: "It's ok! Server is running!" })
+);
 
 export default routes;
