4a69b5b5ddcea9af36982855592c8d2122a19a4a
Valdir Mendes <valdir.mfjesus@yahoo.com.br>
2020-01-30 22:14:07 -0300

    Adding authentication support

---
+++ backend/src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'cb9d9965924e8180352798a6702868d1',
+  expiresIn: '7d',
+};
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,12 @@
 import { Router } from 'express';
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 
 routes.get('/', (req, res) =>
   res.json({ message: "It's ok! Server is running!" })
 );
+
+routes.post('/sessions', SessionController.store);
 
 export default routes;
