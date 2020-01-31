c3931c72076028212d746d356568bd52a4064f19
Valdir Mendes <valdir.mfjesus@yahoo.com.br>
2020-01-30 23:48:39 -0300

    Adding recipients routes structure

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import SessionController from './app/controllers/SessionController';
+import RecipientController from './app/controllers/RecipientController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -11,5 +12,10 @@ routes.get('/', (req, res) =>
 
 routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
+routes.get('/recipients', RecipientController.index);
+routes.post('/recipients', RecipientController.store);
+routes.get('/recipients/:id', RecipientController.show);
+routes.put('/recipients', RecipientController.update);
+routes.delete('/recipients/:id', RecipientController.delete);
 
 export default routes;
