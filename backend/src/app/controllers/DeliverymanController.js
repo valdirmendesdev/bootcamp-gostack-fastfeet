c3244ed054bce6522b805d6d4635e74c175b1d14
Valdir Mendes <valdir.mfjesus@yahoo.com.br>
2020-02-04 00:43:15 -0300

    Adding deliverymen basic routes

---
+++ backend/src/app/controllers/DeliverymanController.js
@@ -0,0 +1,21 @@
+class DeliverymanController {
+
+  async index(req, res){
+    res.json({});
+  }
+  async store(req, res){
+    res.json({});
+  }
+  async show(req, res){
+    res.json({});
+  }
+  async update(req, res){
+    res.json({});
+  }
+  async delete(req, res){
+    res.json({});
+  }
+
+}
+
+export default new DeliverymanController();
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
+import DeliverymanController from './app/controllers/DeliverymanController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -12,10 +13,23 @@ routes.get('/', (req, res) =>
 
 routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
+
+/**
+ * Recipient routes
+ */
 routes.get('/recipients', RecipientController.index);
 routes.post('/recipients', RecipientController.store);
 routes.get('/recipients/:id', RecipientController.show);
 routes.put('/recipients/:id', RecipientController.update);
 routes.delete('/recipients/:id', RecipientController.delete);
+
+/**
+ * Deliveryman routes
+ */
+routes.get('/deliverymen', DeliverymanController.index);
+routes.post('/deliverymen', DeliverymanController.store);
+routes.get('/deliverymen/:id', DeliverymanController.show);
+routes.put('/deliverymen/:id', DeliverymanController.update);
+routes.delete('/deliverymen/:id', DeliverymanController.delete);
 
 export default routes;
